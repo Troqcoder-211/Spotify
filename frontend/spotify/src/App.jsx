93f8f93be65c5bbaaa2c738825b5e2129429832d
hnimgnocmahp <pcongminh551@gmail.com>
2025-04-10 11:13:01 +0700

    f

--- frontend/spotify/src/App.jsx
+++ frontend/spotify/src/App.jsx
@@ -7,9 +7,9 @@ function App() {
   return (
     <>
       {/* <Home /> */}
-      {/* <LoginPage /> */}
+      <LoginPage />
       {/* <RegisterPage /> */}
-      <AdminDashboard />
+      {/* <AdminDashboard /> */}
     </>
   );
 }
