52158c3c70be3fadbe770349c2e0ccd1b5005c42
thhn_trroq <133735405+Troqcoder-211@users.noreply.github.com>
2025-04-29 22:48:04 +0700

    fix explore

--- frontend/spotify/src/components/Navbar.jsx
+++ frontend/spotify/src/components/Navbar.jsx
@@ -52,12 +52,13 @@ const Navbar = () => {
           />
         </div>
         <div className="flex items-center gap-4">
-          <p className="bg-white text-black text-[15px] px-4 py-1 rounded-2xl hidden md:block cursor-pointer">
+          <p
+            onClick={() => navigate("/premium")}
+            className="bg-white text-black text-[15px] px-4 py-1 rounded-2xl hidden md:block cursor-pointer"
+          >
             Explore Premium
           </p>
-          {/* <p className="bg-black py-1 px-3 rounded-2xl text-[15px] cursor-pointer ">
-            Install App
-          </p> */}
+
           {isAuthenticated ? (
             <div className="relative" ref={dropdownRef}>
               <button
